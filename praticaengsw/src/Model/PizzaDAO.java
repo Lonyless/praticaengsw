1b808647eba6271830972539e134b01e27c1083d
Admin <Admin@DESKTOP-T6CJRA6>
2020-10-06 11:01:12 -0300

    del

--- praticaengsw/src/Model/PizzaDAO.java
+++ praticaengsw/src/Model/PizzaDAO.java
@@ -3,6 +3,7 @@ package Model;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PizzaDAO {
 
@@ -19,22 +20,55 @@ public class PizzaDAO {
         return instance;
     }
 
-    public long create(PizzaBEAN pizza) {
-        String query = "INSERT INTO ingrediente (nome, medida, id_fornecedor, status) VALUES (?,?,?,1)";
-        return MySQLDAO.executeQuery(query, ingrediente.getNome(), ingrediente.getMedida(), ingrediente.getId_fornecedor());
+    public long create(PizzaBEAN pizza, List<IngredienteBEAN> ingredienteList) {
+
+        String query = "INSERT INTO aux_pizza_ingrediente (id_pizza,id_ingrediente) VALUES (?,?)";
+
+        for (IngredienteBEAN ingrediente : ingredienteList) {
+            MySQLDAO.executeQuery(query, pizza.getId(), ingrediente.getId());
+        }
+
+        query = "INSERT INTO pizza (nome,detalhes,status) VALUES (?,?,?)";
+
+        return MySQLDAO.executeQuery(query, pizza.getNome(), pizza.getDetalhes(), pizza.getStatus());
     }
 
-    public void update(PizzaBEAN pizza) {
-        String query = "UPDATE ingrediente SET nome=?, medida=?, id_fornecedor=?, status=? WHERE id = ?";
-        MySQLDAO.executeQuery(query, ingrediente.getNome(), ingrediente.getMedida(), ingrediente.getId_fornecedor(), ingrediente.getStatus(), ingrediente.getId());
+    public void update(PizzaBEAN pizza, List<IngredienteBEAN> ingredienteList) {
+
+        String query = "UPDATE pizza SET nome=?, detalhes=?, status=? WHERE id = ?";
+        MySQLDAO.executeQuery(query, pizza.getNome(), pizza.getDetalhes(), pizza.getStatus(), pizza.getId());
+    }
+
+    public ArrayList<IngredienteBEAN> listIngredientePizza(PizzaBEAN pizza) {
+        
+        //select id_ingrediente from aux_pizza_ingrediente join ingrediente on ingrediente.id = aux_pizza_ingrediente.id_ingrediente;
+        
+        ArrayList<IngredienteBEAN> lista = new ArrayList<IngredienteBEAN>();
+        ResultSet rs = null;
+        rs = MySQLDAO.getResultSet("SELECT * FROM ");
+
+        try {
+            while (rs.next()) {
+                lista.add(new IngredienteBEAN(
+                        rs.getInt("id"), rs.getString("nome"), rs.getString("medida"),
+                        rs.getInt("id_fornecedor"), rs.getInt("status")));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        rs = MySQLDAO.getResultSet("SELECT * FROM aux_pizza_ingrediente");
+        
+        
     }
 
     public void delete(PizzaBEAN pizza) {
-        MySQLDAO.executeQuery("DELETE FROM ingrediente WHERE id = ?", ingrediente.getId());
+        MySQLDAO.executeQuery("DELETE FROM ingrediente WHERE id = ?", pizza.getId());
     }
 
-    public ArrayList<PizzaBEAN> findAllIngrediente() {
-        return listaPizza("SELECT * FROM ingrediente ORDER BY id");
+    public ArrayList<PizzaBEAN> findAllPizza() {
+        return listaPizza("SELECT * FROM pizza ORDER BY id");
     }
 
     public ArrayList<PizzaBEAN> listaPizza(String query) {
